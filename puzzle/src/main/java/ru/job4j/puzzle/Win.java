ad7a49ff10e3213c14571145f8201b6cbb83a396
Marsel Yumagulov <marseliumagulov@yandex.ru>
2022-12-06 17:10:27 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -11,6 +11,7 @@ public class Win {
         }
         return result;
     }
+
     public static boolean monoVertical(int[][] board, int column) {
         boolean result = true;
         for (int[] ints : board) {
